ab9e7b15906e8e5152676463d246af98e270cc3c
huuuman12 <vyfydgml10@gmail.com>
2021-01-21 13:48:34 +0900

    First commit

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ var date = require('./myfirstmodule');
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.write("The date and time are currently: " + date.myDateTime());
+  response.write('Hi');
   response.end('Hello world');
 }).listen(8080);
